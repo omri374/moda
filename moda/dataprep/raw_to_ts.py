ddf4f12d18de9283651482bffc5fcda7ca7c7f34
Omri Mendels <omri.mendels@microsoft.com>
2018-09-02 22:38:51 +0300

    added documentation

--- moda/dataprep/raw_to_ts.py
+++ moda/dataprep/raw_to_ts.py
@@ -3,6 +3,11 @@ import pandas as pd
 
 
 def raw_to_ts(raw):
+    """
+    Turns a raw pd.DataFrame into a time-series DataFrame, by creating a DatetimeIndex and a 'timestamp' column
+    :param raw: a pd.DataFrame with a date column
+    :return: a time-series DataFrame
+    """
     if 'date' not in raw:
         raise ValueError("File must contain a date column")
 
--- moda/dataprep/ts_to_range.py
+++ moda/dataprep/ts_to_range.py
@@ -2,6 +2,13 @@ import pandas as pd
 
 
 def ts_to_range(ts, time_range='1H'):
+    """
+    Creates a new data frame with counts per time range
+    :param ts: The original time series
+    :param time_range: The time range requested
+    :return: a pd.DataFrame with a MultiIndex containing a date and category. Contains an additional column with
+    counts in the interval
+    """
     if not isinstance(ts.index, pd.core.indexes.datetimes.DatetimeIndex):
         print('Wrong index type. Expecting pd.core.indexes.datetimes.DateTimeIndex')
         return
