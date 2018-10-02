68251713dbf70bf15af7a76691a55e8957f31b4d
Omri Mendels <omri.mendels@microsoft.com>
2018-10-02 15:50:34 +0300

    removed old keras code

--- moda/models/lstm/lstm.py
+++ moda/models/lstm/lstm.py
@@ -1,9 +1,9 @@
 # import comet_ml in the top of your file
 from comet_ml import Experiment
-from keras.layers import BatchNormalization
 
 from moda.dataprep.create_dataset import get_windowed_ts, split_history_and_current
 from moda.example.example import prep_data
+from moda.models.trend_detector import AbstractTrendDetector
 
 experiment = Experiment(api_key="Uv0lx3yRDH7kk8h1vtR9ZRiD2s16gnYTxfsvK2VnpV2xRrMbFobYDZRRA4tvoYiR",
                         project_name="keras-lstm-sf311-forecast", workspace="omri374")
@@ -11,24 +11,75 @@ experiment = Experiment(api_key="Uv0lx3yRDH7kk8h1vtR9ZRiD2s16gnYTxfsvK2VnpV2xRrM
 import pandas as pd
 
 
-def lstm_forecast(window_size):
+class LSTMTrendinessDetector(AbstractTrendDetector):
+    __name__ = 'LSTMTrendinessDetector'
+    """An detector for anomalies on time series using an LSTM time series forecasting model
+
+    Parameters
+    ----------
+    is_multicategory : boolean
+        Whether the provided dataset contains multiple categories per time stamp (True), or just one value per timestamp (False)
+    num_of_std : float
+        Sets the threshold for anomalies using the median + std*num_of_std rule, either for trends or residuals
+    freq : String
+        The time-series frequency, used to fill missing date/times. See https://pandas.pydata.org/pandas-docs/stable/timeseries.html
+
+    seasonality : integer
+        The seasonality of the data. See http://www.statsmodels.org/dev/generated/statsmodels.tsa.seasonal.seasonal_decompose.html
+
+    anomaly_type: String
+        The values on which anomalies are looked for. Possible values are ['trend', 'residual', 'and', 'or']
+
+    resample : bool
+        Whether to add missing dates/times to time series prior to modeling
+
+    min_periods : numeric
+        The minimum value of periods for which to attemp to model. A time series shorter than that will return prediction = 0 
+
+    lookback : String
+        The time for which statistics for a specific timestamp look back. i.e. A sliding window for statistics (median, std). Example: '30D','12H' etc.
+    """
+
+    def __init__(self, freq, is_multicategory=True, num_of_std=3, min_value=None,window_size = 24,
+                 resample=True, min_periods=10, lookback='30D'):
+        super(LSTMTrendinessDetector, self).__init__(freq, is_multicategory, resample)
+        self.lookback = lookback
+        self.num_of_std = num_of_std
+        self.min_value = min_value
+        self.min_periods = min_periods
+        self.window_size = window_size
+
+
+def fit(self, X, y=None, verbose=False):
+
+    X_scaled = scale(X)
+
+
+    ts = get_windowed_ts(X_scaled)
+    train_X, train_y = split_history_and_current(ts)
+
+
+
+
+
+
+
+def lstm_forecast_model(window_size):
     from keras.layers.recurrent import LSTM
     from keras.models import Sequential
-    from keras.layers import Conv1D
     from keras.layers.core import Dense, Activation
 
     model = Sequential()
-    model.add(Conv1D(input_shape=(window_size, 1), filters=32, kernel_size=10))
-    model.add(BatchNormalization())
-    model.add(LSTM(output_dim=window_size, return_sequences=True))
-    model.add(LSTM(32, return_sequences=False))
+    # model.add(Conv1D(input_shape=(window_size, 1), filters=32, kernel_size=10))
+    # model.add(MaxPooling1D(pool_size=5))
+    model.add(LSTM(input_shape=(window_size, 1), output_dim=window_size, return_sequences=True))
+    model.add(LSTM(128, return_sequences=False))
     model.add(Dense(1))
     model.add(Activation("linear"))
     model.compile(loss="mse", optimizer="adam")
-    print(model.summary())
+    model.summary()
 
     return model
-
 
 def scale(X):
     from sklearn.preprocessing import MinMaxScaler
@@ -40,6 +91,8 @@ def scale(X):
     scaled['value'] = scaled_values
 
     return scaled
+
+
 
 
 if __name__ == '__main__':
@@ -71,7 +124,7 @@ if __name__ == '__main__':
 
     train_X = train_X.reshape(train_X.shape[0], train_X.shape[1], 1)
     test_X = test_X.reshape(test_X.shape[0], test_X.shape[1], 1)
-    model = lstm_forecast(window_size=window_size)
+    model = lstm_forecast_model(window_size=window_size)
 
     import time
 
@@ -88,4 +141,4 @@ if __name__ == '__main__':
     prediction = preds
     mse = mean_squared_error(actual, prediction)
     print("MSE=" + str(mse))
-    experiment.log_metric("mse",str(mse))
+    experiment.log_metric("mse", str(mse))
